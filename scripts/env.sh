1a84feb270d0719e71576f7ea2c009449e1209d0
Randy Gelhausen <rgelhau@gmail.com>
2017-11-02 02:30:44 -0400

    Update env.sh
    fixed local buildserver urls

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,13 +1,13 @@
-#ZOOKEEPER_URL=https://archive.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz
-ZOOKEEPER_URL=build:8000/zookeeper-3.4.6.tar.gz
+ZOOKEEPER_URL=https://archive.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz
+#ZOOKEEPER_URL=build:8000/zookeeper-3.4.6.tar.gz
 ZOOKEEPER_SUBFOLDER=zookeeper-3.4.6
 
-#DRUID_URL=http://static.druid.io/artifacts/releases/druid-0.10.1-bin.tar.gz
-#DRUID_SUBFOLDER=druid-0.10.1
-DRUID_URL=build:8000/druid-0.11.1-SNAPSHOT-bin.tar.gz
-DRUID_SUBFOLDER=druid-0.11.1-SNAPSHOT
+DRUID_URL=http://static.druid.io/artifacts/releases/druid-0.10.1-bin.tar.gz
+DRUID_SUBFOLDER=druid-0.10.1
+#DRUID_URL=build:8000/druid-0.11.1-SNAPSHOT-bin.tar.gz
+#DRUID_SUBFOLDER=druid-0.11.1-SNAPSHOT
 
-#TRANQUILITY_URL=http://static.druid.io/tranquility/releases/tranquility-distribution-0.8.0.tgz
-#TRANQUILITY_SUBFOLDER=tranquility-distribution-0.8.0
-TRANQUILITY_URL=build:8000/tranquility-distribution-0.9.0-SNAPSHOT.tgz
-TRANQUILITY_SUBFOLDER=tranquility-distribution-0.9.0-SNAPSHOT
+TRANQUILITY_URL=http://static.druid.io/tranquility/releases/tranquility-distribution-0.8.0.tgz
+TRANQUILITY_SUBFOLDER=tranquility-distribution-0.8.0
+#TRANQUILITY_URL=build:8000/tranquility-distribution-0.9.0-SNAPSHOT.tgz
+#TRANQUILITY_SUBFOLDER=tranquility-distribution-0.9.0-SNAPSHOT
